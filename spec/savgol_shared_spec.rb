3dcca239437f5516505ac90dfe09389396a0c308
John Prince <jtprince@gmail.com>
2014-03-26 23:45:54 -0600

    worked out shared spec

--- spec/savgol_shared_spec.rb
+++ spec/savgol_shared_spec.rb
@@ -1,39 +1,36 @@
 require 'spec_helper'
 
-# describe Savgol do
-# 
-#   describe 'padding the ends' do
-# 
-#     subject do
-#       ar = [1, 2, 3, 4, -7, -2, 0, 1, 1]
-#       ar.extend(Savgol)
-#       ar
-#     end
-# 
-#     it 'pads with the reverse geometrically inverted sequence' do
-#       subject.sg_pad_ends(2).should == [-1, 0, 1, 2, 3, 4, -7, -2, 0, 1, 1, 1, 2]
-#       subject.sg_pad_ends(3).should == [-2, -1, 0, 1, 2, 3, 4, -7, -2, 0, 1, 1, 1, 2, 4]
-#     end
-#   end
-# 
-#   describe 'smoothing a signal' do
-#     subject do
-#       ar = [1, 2, 3, 4, 3.5, 5, 3, 2.2, 3, 0, -1, 2, 0, -2, -5, -8, -7, -2, 0, 1, 1]
-#       ar.extend(Savgol)
-#       ar
-#     end
-# 
-#     it "works for the simple case" do
-#       numpy_savgol_output = [1.0, 2.0, 3.12857143, 3.57142857, 4.27142857, 4.12571429, 3.36857143, 2.69714286, 2.04, 0.32571429, -0.05714286, 0.8, 0.51428571, -2.17142857, -5.25714286, -7.65714286, -6.4, -2.77142857, 0.17142857, 0.91428571, 1.0]
-#       sg = ar.savgol(5,3)
-#       sg.size.should == numpy_savgol_output.size
-#       sg.zip(numpy_savgol_output) do |sgv, numpy_sgv|
-#         sgv.should be_within(0.000001).of(numpy_sgv)
-#       end
-#     end
-#   end
-# end
+shared_examples "a savgol smoother" do
+  let(:smoother) do 
+    object = described_class[1, 2, 3, 4, -7, -2, 0, 1, 1]
+    object.extend(Savgol)
+    object
+  end
 
+  it 'pads with the reverse geometrically inverted sequence' do
+    expect(smoother.sg_pad_ends(2)).to eq [-1, 0, 1, 2, 3, 4, -7, -2, 0, 1, 1, 1, 2]
+    expect(smoother.sg_pad_ends(3)).to eq [-2, -1, 0, 1, 2, 3, 4, -7, -2, 0, 1, 1, 1, 2, 4]
+  end
+
+  describe 'smoothing a signal' do
+    let(:smoother) do
+      object = described_class[1, 2, 3, 4, 3.5, 5, 3, 2.2, 3, 0, -1, 2, 0, -2, -5, -8, -7, -2, 0, 1, 1]
+      object.extend(Savgol)
+      object
+    end
+
+    it "works for the simple case" do
+      numpy_savgol_output = [1.0, 2.0, 3.12857143, 3.57142857, 4.27142857, 4.12571429, 3.36857143, 2.69714286, 2.04, 0.32571429, -0.05714286, 0.8, 0.51428571, -2.17142857, -5.25714286, -7.65714286, -6.4, -2.77142857, 0.17142857, 0.91428571, 1.0]
+      sg = smoother.savgol(5,3)
+      sg.size.should == numpy_savgol_output.size
+
+      numpy_savgol_output.each_with_index do |exp, i|
+        expect(sg[i]).to be_within(0.000001).of(exp)
+      end
+    end
+  end
+
+end
 
 =begin
     b = matrix([[ 1, -2,  4, -8],
@@ -89,7 +86,6 @@ array([ 1.        ,  2.        ,  3.12857143,  3.57142857,  4.27142857,
         4.12571429,  3.36857143,  2.69714286,  2.04      ,  0.32571429,
        -0.05714286,  0.8       ,  0.51428571, -2.17142857, -5.25714286,
        -7.65714286, -6.4       , -2.77142857,  0.17142857,  0.91428571,  1.        ])
-
 
 =end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,9 @@
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'rspec'
-require 'savgol'
+require 'simplecov'
+SimpleCov.start
 
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+require 'rspec'
 
 RSpec.configure do |config|
-  
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.color = true
 end
